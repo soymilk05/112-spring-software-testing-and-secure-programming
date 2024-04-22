838155ed49f00855f61e68064bdb5192c750d542
soymilk05 <chifang.cs12@nycu.edu.tw>
2024-04-22 23:08:46 +0800

    422

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -1,22 +1,34 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    // Launch the browser and open a new blank page
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-
-    // Navigate the page to a URL
     await page.goto('https://pptr.dev/');
 
-    // Hints:
-    // Click search button
-    // Type into search box
-    // Wait for search result
-    // Get the `Docs` result section
-    // Click on first result in `Docs` section
-    // Locate the title
-    // Print the title
+    await page.setViewport({width: 1080, height: 1024});
 
-    // Close the browser
+    await page.waitForSelector('.DocSearch-Button');
+    await page.click('.DocSearch-Button');
+
+    await page.waitForSelector('#docsearch-input');
+    await page.type('#docsearch-input',"chipi chipi chapa chapa")
+
+    await new Promise((resolve)=>{
+        setTimeout((resolve),3000)
+    })
+
+    await page.waitForSelector('#docsearch-item-5 a');
+    await page.click('#docsearch-item-5 a');
+
+
+    await new Promise((resolve)=>{
+        setTimeout((resolve),3000)
+    })
+
+    const element =  await page.waitForSelector('h1');
+    const title = await element.evaluate((ele)=>{
+        return ele.textContent
+    })
+    console.log(title);
     await browser.close();
 })();
